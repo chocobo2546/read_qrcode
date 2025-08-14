172dcea8a17f4be69d8ed4029abf61be1e2ce85c
Pattrawut Keawkum <pattrawut.make@gmail.com>
2025-08-14 10:09:03 +0700

    Delete qrcodeservice directory

--- qrcodeservice/src/main/java/th/co/scgp/qrcodeservice/service/QrCodeService.java
+++
@@ -1,8 +0,0 @@
-package th.co.scgp.qrcodeservice.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface QrCodeService {
-    String readQrCode(MultipartFile file) throws Exception;
-
-}
--- qrcodeservice/src/main/java/th/co/scgp/qrcodeservice/service/QrDataParserService.java
+++
@@ -1,8 +0,0 @@
-package th.co.scgp.qrcodeservice.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface QrDataParserService {
-    String parseQrData(String input);
-}
-
